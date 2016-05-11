e189a3d5e277d3244c926f8015fb8aa27a78c135
jiazeng <sophbj93@uw.edu>
2016-05-10 20:43:34 -0700

    class work

--- exercise-3/js/Calc.js
+++ exercise-3/js/Calc.js
@@ -2,12 +2,43 @@ window.Calc = (function() {
     // Write a constructor function which takes a number, stores it, and
     // defines all Calc API functions
     var c = function(num) {
-        // Find a way to store all instance viariables and functions
-        // for this calculator
+        var calc = {};
+        calc.currNum = num;
 
         // Implement getVal, plus, minus, times, divide, and clear
+        calc.getVal = function() {
+            return this.currNum;
+        }
+        
+        calc.plus = function(n) {
+            this.currNum += n;
+            return this;
+        }
+
+        calc.minus = function(n) {
+            this.currNum -= n;
+            return this;
+        }
+
+        calc.times = function(n) {
+            this.currNum *= n;
+            return this;
+        }
+
+        calc.divide = function(n) {
+            this.currNum /= n;
+            return this;
+        }
+
+        calc.clear = function() {
+            this.currNum = 0;
+            return this;
+        }
+
+        return calc;
     };
 
     // Return your constructed object
     return c;
 })();
+Status API Training Shop Blog About
